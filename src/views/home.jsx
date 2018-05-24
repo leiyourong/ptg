37169eeab288193b20a0991a5174471326c48aaa
leiyourong <460951178@qq.com>
2018-05-24 22:14:20 +0800

    react16?

--- src/views/home.jsx
+++ src/views/home.jsx
@@ -2,8 +2,6 @@ import React, { Component } from 'react'
 
 export default class home extends Component {
   render () {
-    return (
-      <div>It's Home</div>
-    )
+    return [<div key='1'>It's Home</div>, <div key='2'>It's Home2</div>]
   }
 }
