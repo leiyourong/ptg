a31615818e94eca4e92b945d8b748e1d491b23a3
leiyourong <460951178@qq.com>
2019-05-26 00:02:31 +0800

    remove createAction & handleAction

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,20 +1,41 @@
-import { createActions } from 'redux-actions'
-import { getBooks, deleteBooks, addBook, editBook } from '../services/index'
-export const DELETE_ITEM = 'DELETE_ITEM'
-export const ADD_ITEM = 'ADD_ITEM'
-export const GET_ITEM = 'GET_ITEM'
-export const EDIT_ITEM = 'EDIT_ITEM'
+// import { createActions } from 'redux-actions'
+import {getBooks, deleteBooks, addBook, editBook} from '../services/index';
+import {DELETE_ITEM, ADD_ITEM, GET_ITEM, EDIT_ITEM} from '../constants/index';
 
-const { deleteItem, getItem, addItem, editItem } = createActions({
-  DELETE_ITEM: payload => deleteBooks(payload),
-  GET_ITEM: () => getBooks(),
-  ADD_ITEM: payload => addBook(payload),
-  EDIT_ITEM: payload => editBook(payload)
-})
+// const { deleteItem, editItem } = createActions({
+//   DELETE_ITEM: payload => deleteBooks(payload),
+//   GET_ITEM: () => getBooks(),
+//   ADD_ITEM: payload => addBook(payload),
+//   EDIT_ITEM: payload => editBook(payload)
+// })
 
 export default {
-  deleteItem,
-  getItem,
-  addItem,
-  editItem
+  deleteItem: payload => async dispatch => {
+    const book = await deleteBooks(payload);
+    dispatch({
+      type: DELETE_ITEM,
+      data: book,
+    })
+  },
+  getItem: () => async dispatch => {
+    const books = await getBooks();
+    dispatch({
+      type: GET_ITEM,
+      data: books,
+    })
+  },
+  addItem: payload => async dispatch => {
+    const book = await addBook(payload);
+    dispatch({
+      type: ADD_ITEM,
+      data: book,
+    })
+  },
+  editItem: payload => async dispatch => {
+    const book = await editBook(payload);
+    dispatch({
+      type: EDIT_ITEM,
+      data: book,
+    })
+  }
 }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,35 +1,60 @@
-import { handleActions } from 'redux-actions'
+// import { handleActions } from 'redux-actions'
 import { combineReducers } from 'redux'
+import {DELETE_ITEM, ADD_ITEM, GET_ITEM, EDIT_ITEM} from '../constants/index';
 
+import Immutable from 'immutable';
 const initState = {
   books: []
 }
 
-const items = handleActions({
-  DELETE_ITEM: (state, action) => {
-    let books = state.books
-    books = books.filter(book => (book.id !== action.payload.id))
-    return Object.assign({}, state, { books })
-  },
-  GET_ITEM: (state, action) => {
-    let books = action.payload.items
-    return Object.assign({}, state, { books })
-  },
-  ADD_ITEM: (state, action) => {
-    return Object.assign({}, state, { books: [ ...state.books, action.payload ] })
-  },
-  EDIT_ITEM: (state, action) => {
-    let books = state.books
-    const { id, name, price } = action.payload
-    books = books.map(book => {
-      if (book.id === id) {
-        Object.assign(book, { name, price })
-      }
-      return book
-    })
-    return Object.assign({}, state, { books })
+const items = (state = initState, payload) => {
+  let books
+  switch (payload.type) {
+    case GET_ITEM: 
+      books = payload.data.items;
+      return Object.assign({}, state, { books });
+    case ADD_ITEM: 
+      return Object.assign({}, state, { books: [ ...state.books, payload.data ] })
+    case DELETE_ITEM: 
+      books = state.books.filter(book => (book.id !== payload.data.id))
+      return Object.assign({}, state, { books })
+    case EDIT_ITEM: 
+      const { id, name, price } = payload.data
+      books = state.books.map(book => {
+        if (book.id === id) {
+          Object.assign(book, { name, price })
+        }
+        return book
+      })
+      return Object.assign({}, state, { books })
   }
-}, initState)
+  return state;
+}
+// const items = handleActions({
+//   DELETE_ITEM: (state, action) => {
+//     let books = state.books
+//     books = books.filter(book => (book.id !== action.payload.id))
+//     return Object.assign({}, state, { books })
+//   },
+//   GET_ITEM: (state, action) => {
+//     let books = action.payload.items
+//     return Object.assign({}, state, { books })
+//   },
+//   ADD_ITEM: (state, action) => {
+//     return Object.assign({}, state, { books: [ ...state.books, action.payload ] })
+//   },
+//   EDIT_ITEM: (state, action) => {
+//     let books = state.books
+//     const { id, name, price } = action.payload
+//     books = books.map(book => {
+//       if (book.id === id) {
+//         Object.assign(book, { name, price })
+//       }
+//       return book
+//     })
+//     return Object.assign({}, state, { books })
+//   }
+// }, initState)
 
 export default combineReducers({
   items
