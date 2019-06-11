0e145c78bac3701bb41e758702c6dda7611ad6f9
Ray <Ray.lei@ringcentral.com>
2019-06-11 10:18:58 +0800

    ...

--- jest.config.js
+++ jest.config.js
@@ -141,8 +141,8 @@ module.exports = {
 
   // The glob patterns Jest uses to detect test files
   testMatch: [
-    "**/__tests__/**/*.[jt]s?(x)",
-    "**/?(*.)+(spec|test).[tj]s?(x)"
+    // "**/__tests__/**/*.[jt]s?(x)",
+    "**/unit/**/*.test.[jt]s?(x)"
   ],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
---
+++ test/unit/promise.test.js
@@ -0,0 +1,7 @@
+import Promise from '../../src/promise/index';
+
+describe('promise constructor', async () => {
+    it('should receive a function and initialize with new', () => {
+        
+    })
+});
